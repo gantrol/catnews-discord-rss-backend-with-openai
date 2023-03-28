054c2ad22230678d0070764147f7b30de6a21f2c
gantrol <gantrol.h@qq.com>
2023-03-28 21:03:34 +0800

    modified: 调整模型，微调参数

--- app/utils/aiapi.py
+++ app/utils/aiapi.py
@@ -5,12 +5,12 @@ from app.config import settings
 
 openai.api_key = settings.OPEN_AI_KEY
 
-# TODO: text 的取值，然后是openai的调用
+
 def generate_tags_and_summary(text: str) -> Tuple[List[str], str]:
     # Generate tags
     sep = ","
     tags_response = openai.Completion.create(
-        engine="text-davinci-002",
+        engine="text-davinci-003",
         prompt=f"Generate 3 tags for the following text, split with {sep}:\n{text}",
         max_tokens=50,
         n=1,
@@ -20,9 +20,9 @@ def generate_tags_and_summary(text: str) -> Tuple[List[str], str]:
     tags = [tag.strip() for tag in tags_response.choices[0].text.split(sep)]
     # Generate summary
     summary_response = openai.Completion.create(
-        engine="text-davinci-002",
+        engine="text-davinci-003",
         prompt=f"Create a summary of the following text:\n{text}",
-        max_tokens=100,
+        max_tokens=200,
         n=1,
         stop=None,
         temperature=0.5,
